82a7b2c20d8db016e8066f8f7a499e7b48d6b5fd
kelvinsanchez15 <kelvinsanchez15@gmail.com>
2020-08-31 15:56:35 -0400

    complete all test cases

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -14,76 +14,115 @@ suite("Unit Tests", () => {
     });
   });
 
-  suite("Function ____()", () => {
+  suite("Function translate()", () => {
     suite("American to British English", () => {
       test("Mangoes are my favorite fruit. --> Mangoes are my favourite fruit.", (done) => {
         const input = "Mangoes are my favorite fruit.";
         const output = "Mangoes are my favourite fruit.";
+        const translationType = "american-to-british";
 
-        // done();
+        const translation = Translator.translate(input, translationType)[0];
+        assert.strictEqual(translation, output);
+        done();
       });
 
       test("I ate yogurt for breakfast. --> I ate yoghurt for breakfast.", (done) => {
         const input = "I ate yogurt for breakfast.";
         const output = "I ate yoghurt for breakfast.";
 
-        // done();
+        const translationType = "american-to-british";
+
+        const translation = Translator.translate(input, translationType)[0];
+        assert.strictEqual(translation, output);
+        done();
       });
 
       test("We had a party at my friend's condo. --> We had a party at my friend's flat.", (done) => {
         const input = "We had a party at my friend's condo.";
         const output = "We had a party at my friend's flat.";
 
-        // done();
+        const translationType = "american-to-british";
+
+        const translation = Translator.translate(input, translationType)[0];
+        assert.strictEqual(translation, output);
+        done();
       });
 
       test("Can you toss this in the trashcan for me? --> Can you toss this in the bin for me?", (done) => {
         const input = "Can you toss this in the trashcan for me?";
         const output = "Can you toss this in the bin for me?";
 
-        // done();
+        const translationType = "american-to-british";
+
+        const translation = Translator.translate(input, translationType)[0];
+        assert.strictEqual(translation, output);
+        done();
       });
 
       test("The parking lot was full. --> The car park was full.", (done) => {
         const input = "The parking lot was full.";
         const output = "The car park was full.";
 
-        // done();
+        const translationType = "american-to-british";
+
+        const translation = Translator.translate(input, translationType)[0];
+        assert.strictEqual(translation, output);
+        done();
       });
 
       test("Like a high tech Rube Goldberg machine. --> Like a high tech Heath Robinson device.", (done) => {
         const input = "Like a high tech Rube Goldberg machine.";
         const output = "Like a high tech Heath Robinson device.";
 
-        // done();
+        const translationType = "american-to-british";
+
+        const translation = Translator.translate(input, translationType)[0];
+        assert.strictEqual(translation, output);
+        done();
       });
 
       test("To play hooky means to skip class or work. --> To bunk off means to skip class or work.", (done) => {
         const input = "To play hooky means to skip class or work.";
         const output = "To bunk off means to skip class or work.";
 
-        // done();
+        const translationType = "american-to-british";
+
+        const translation = Translator.translate(input, translationType)[0];
+        assert.strictEqual(translation, output);
+        done();
       });
 
       test("No Mr. Bond, I expect you to die. --> No Mr Bond, I expect you to die. ", (done) => {
         const input = "No Mr. Bond, I expect you to die.";
         const output = "No Mr Bond, I expect you to die.";
 
-        // done();
+        const translationType = "american-to-british";
+
+        const translation = Translator.translate(input, translationType)[0];
+        assert.strictEqual(translation, output);
+        done();
       });
 
       test("Dr. Grosh will see you now. --> Dr Grosh will see you now. ", (done) => {
         const input = "Dr. Grosh will see you now.";
         const output = "Dr Grosh will see you now.";
 
-        // done();
+        const translationType = "american-to-british";
+
+        const translation = Translator.translate(input, translationType)[0];
+        assert.strictEqual(translation, output);
+        done();
       });
 
       test("Lunch is at 12:15 today. --> Lunch is at 12.15 today.", (done) => {
         const input = "Lunch is at 12:15 today.";
         const output = "Lunch is at 12.15 today.";
 
-        // done();
+        const translationType = "american-to-british";
+
+        const translation = Translator.translate(input, translationType)[0];
+        assert.strictEqual(translation, output);
+        done();
       });
     });
 
@@ -92,70 +131,110 @@ suite("Unit Tests", () => {
         const input = "We watched the footie match for a while.";
         const output = "We watched the soccer match for a while.";
 
-        // done();
+        const translationType = "british-to-american";
+
+        const translation = Translator.translate(input, translationType)[0];
+        assert.strictEqual(translation, output);
+        done();
       });
 
       test("Paracetamol takes up to an hour to work. --> Tylenol takes up to an hour to work.", (done) => {
         const input = "Paracetamol takes up to an hour to work.";
         const output = "Tylenol takes up to an hour to work.";
 
-        // done();
+        const translationType = "british-to-american";
+
+        const translation = Translator.translate(input, translationType)[0];
+        assert.strictEqual(translation, output);
+        done();
       });
 
       test("First, caramelise the onions. --> First, caramelize the onions.", (done) => {
         const input = "First, caramelise the onions.";
         const output = "First, caramelize the onions.";
 
-        // done();
+        const translationType = "british-to-american";
+
+        const translation = Translator.translate(input, translationType)[0];
+        assert.strictEqual(translation, output);
+        done();
       });
 
       test("I spent the bank holiday at the funfair. --> I spent the public holiday at the carnival.", (done) => {
         const input = "I spent the bank holiday at the funfair.";
         const output = "I spent the public holiday at the carnival.";
 
-        // done();
+        const translationType = "british-to-american";
+
+        const translation = Translator.translate(input, translationType)[0];
+        assert.strictEqual(translation, output);
+        done();
       });
 
       test("I had a bicky then went to the chippy. --> I had a cookie then went to the fish-and-chip shop.", (done) => {
         const input = "I had a bicky then went to the chippy.";
         const output = "I had a cookie then went to the fish-and-chip shop.";
 
-        // done();
+        const translationType = "british-to-american";
+
+        const translation = Translator.translate(input, translationType)[0];
+        assert.strictEqual(translation, output);
+        done();
       });
 
       test("I've just got bits and bobs in my bum bag. --> I've just got odds and ends in my fanny pack.", (done) => {
         const input = "I've just got bits and bobs in my bum bag.";
         const output = "I've just got odds and ends in my fanny pack.";
 
-        // done();
+        const translationType = "british-to-american";
+
+        const translation = Translator.translate(input, translationType)[0];
+        assert.strictEqual(translation, output);
+        done();
       });
 
       test("The car boot sale at Boxted Airfield was called off. --> The swap meet at Boxted Airfield was called off.", (done) => {
         const input = "The car boot sale at Boxted Airfield was called off.";
         const output = "The swap meet at Boxted Airfield was called off.";
 
-        // done();
+        const translationType = "british-to-american";
+
+        const translation = Translator.translate(input, translationType)[0];
+        assert.strictEqual(translation, output);
+        done();
       });
 
       test("Have you met Mrs Kalyani? --> Have you met Mrs. Kalyani?", (done) => {
         const input = "Have you met Mrs Kalyani?";
         const output = "Have you met Mrs. Kalyani?";
 
-        // done();
+        const translationType = "british-to-american";
+
+        const translation = Translator.translate(input, translationType)[0];
+        assert.strictEqual(translation, output);
+        done();
       });
 
       test("Prof Joyner of King's College, London. --> Prof. Joyner of King's College, London.", (done) => {
         const input = "Prof Joyner of King's College, London.";
         const output = "Prof. Joyner of King's College, London.";
 
-        // done();
+        const translationType = "british-to-american";
+
+        const translation = Translator.translate(input, translationType)[0];
+        assert.strictEqual(translation, output);
+        done();
       });
 
       test("Tea time is usually around 4 or 4.30. --> Tea time is usually around 4 or 4:30.", (done) => {
-        const input = "Lunch is at 12:15 today.";
-        const output = "Lunch is at 12.15 today.";
+        const input = "Tea time is usually around 4 or 4.30.";
+        const output = "Tea time is usually around 4 or 4:30.";
 
-        // done();
+        const translationType = "british-to-american";
+
+        const translation = Translator.translate(input, translationType)[0];
+        assert.strictEqual(translation, output);
+        done();
       });
     });
   });
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -8,42 +8,109 @@ suite("Functional Tests", () => {
     Translator = require("../public/translator.js");
   });
 
-  suite("Function ____()", () => {
-    /* 
+  suite("Function translateClickHandler()", () => {
+    test("Translation appended to the `translated-sentence` `div`", (done) => {
+      /*
       The translated sentence is appended to the `translated-sentence` `div`
-      and the translated words or terms are wrapped in 
+      and the translated words or terms are wrapped in
       `<span class="highlight">...</span>` tags when the "Translate" button is pressed.
     */
-    test("Translation appended to the `translated-sentence` `div`", (done) => {
-      // done();
+      const input = "Play guitar is my favorite hobby";
+      const expected =
+        'Play guitar is my <span class="highlight">favourite</span> hobby';
+
+      const textInput = document.querySelector("#text-input");
+      const localeSelect = document.querySelector("#locale-select");
+      const translationDiv = document.querySelector("#translated-sentence");
+
+      textInput.value = input;
+      localeSelect.value = "american-to-british";
+
+      Translator.translateClickHandler();
+
+      assert.strictEqual(translationDiv.innerHTML, expected);
+
+      done();
     });
 
-    /* 
+    test("'Everything looks good to me!' message appended to the `translated-sentence` `div`", (done) => {
+      /*
       If there are no words or terms that need to be translated,
       the message 'Everything looks good to me!' is appended to the
       `translated-sentence` `div` when the "Translate" button is pressed.
     */
-    test("'Everything looks good to me!' message appended to the `translated-sentence` `div`", (done) => {
-      // done();
+      const input = "A test string with nothing to translate";
+      const expected = "Everything looks good to me!";
+
+      const textInput = document.querySelector("#text-input");
+      const localeSelect = document.querySelector("#locale-select");
+      const translationDiv = document.querySelector("#translated-sentence");
+
+      textInput.value = input;
+      localeSelect.value = "american-to-british";
+
+      Translator.translateClickHandler();
+
+      assert.strictEqual(translationDiv.innerHTML, expected);
+
+      done();
     });
 
-    /* 
+    test("'Error: No text to translate.' message appended to the `error-msg` `div`", (done) => {
+      /*
       If the text area is empty when the "Translation" button is
-      pressed, append the message 'Error: No text to translate.' to 
+      pressed, append the message 'Error: No text to translate.' to
       the `error-msg` `div`.
     */
-    test("'Error: No text to translate.' message appended to the `translated-sentence` `div`", (done) => {
-      // done();
+      const input = "";
+      const expected = "Error: No text to translate.";
+
+      const textInput = document.querySelector("#text-input");
+      const localeSelect = document.querySelector("#locale-select");
+      const errorDiv = document.querySelector("#error-msg");
+
+      textInput.value = input;
+      localeSelect.value = "american-to-british";
+
+      Translator.translateClickHandler();
+
+      assert.strictEqual(errorDiv.innerText, expected);
+
+      done();
     });
   });
 
-  suite("Function ____()", () => {
-    /* 
+  suite("Function clearClickHandler()", () => {
+    test("Text area, `translated-sentence`, and `error-msg` are cleared", (done) => {
+      /*
       The text area and both the `translated-sentence` and `error-msg`
       `divs` are cleared when the "Clear" button is pressed.
     */
-    test("Text area, `translated-sentence`, and `error-msg` are cleared", (done) => {
-      // done();
+      const input = "Play guitar is my favorite hobby";
+      const expected =
+        'Play guitar is my <span class="highlight">favourite</span> hobby';
+
+      const textInput = document.querySelector("#text-input");
+      const localeSelect = document.querySelector("#locale-select");
+      const translationDiv = document.querySelector("#translated-sentence");
+      const errorDiv = document.querySelector("#error-msg");
+
+      // Mock translation
+      textInput.value = input;
+      localeSelect.value = "american-to-british";
+
+      Translator.translateClickHandler();
+
+      assert.strictEqual(translationDiv.innerHTML, expected);
+
+      // Clear All
+      Translator.clearClickHandler();
+
+      assert.strictEqual(textInput.value, "");
+      assert.strictEqual(translationDiv.innerHTML, "");
+      assert.strictEqual(errorDiv.textContent, "");
+
+      done();
     });
   });
 });
